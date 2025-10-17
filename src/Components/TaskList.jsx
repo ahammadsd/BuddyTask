2523b269f35f40ca009b3c2a2228b6f0d602d512
ahammadsd <sdahamadbasha8@gmail.com>
2025-10-17 15:06:16 +0530

    Tasklist and Progress

--- src/Components/TaskList.jsx
+++ src/Components/TaskList.jsx
@@ -1,7 +1,30 @@
-export default function TaskList() {
+export default function TaskList({ tasks, updateTask, deleteTask }) {
+
+  const toggleComplete = (index) => {
+    const updatedTask = { ...tasks[index], completed: !tasks[index].completed };
+    updateTask(updatedTask, index);
+  };
+
   return (
-    <div>
-      <h1>TaskList Component</h1>
+    <div className='task-list'>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index} className= {task.completed ? 'completed': ''}>
+            <div>
+              <span>
+                {task.text}
+              </span>
+              <small> ({task.priority}, {task.category})</small>
+            </div>
+            <button onClick={() => toggleComplete(index)}>
+              {task.completed ? 'Undo' : 'Complete'}
+            </button>
+            <button onClick={() => deleteTask(index)}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }

b650382048af243e0d93ab02262653ec5c32f581
ahammadsd <sdahamadbasha8@gmail.com>
2025-10-17 13:23:54 +0530

    16th oct Complete Transform

--- src/Components/TaskForm.jsx
+++ src/Components/TaskForm.jsx
@@ -1,24 +1,45 @@
-export default function Taskform() {
-    return(
-        <form>
-            <div>
-                <input type="text" placeholder="Enter the task"/>
-                <button type="submit">Add Task</button>
-            </div>
+import { useState } from "react"
 
-            <div>
-                <select>
-                    <option value="high">High</option>
-                    <option value="medium">Medium</option>
-                    <option value="low">low</option>
-                </select>
+export default function Taskform({addTask}) {
+  const [task, setTask] = useState('');
+  const [priority, setPriority] = useState('medium');
+  const [category, setCategory] = useState('General');
 
-                <select>
-                    <option value="general">General</option>
-                    <option value="work">work</option>
-                    <option value="personal">Personal</option>
-                </select>
-            </div>
-        </form>
-    )
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    addTask({text: task, priority, category, completed: false});
+
+    //reset
+    setTask('');
+    setPriority("medium");
+    setCategory("General");
+  }
+
+  return (
+      <form onSubmit={handlesubmit} className='task-form'> 
+        <div id="inp">
+          <input type='text' placeholder='Enter the task'
+           value={task}
+          onChange={(e)=> setTask(e.target.value)}/>
+          <button type="submit">Add task</button>
+        </div>
+
+      <div id='btns'>
+      <select value={priority} onChange={(e)=> setPriority(e.target.value)}>
+        <option value="High">High</option>
+        <option value="Medium">Medium</option>
+        <option value="low">low</option>
+      </select>
+
+      <select value={category} onChange={(e)=>setCategory(e.target.value)}>
+        <option value="general">General</option>
+        <option value="work">work</option>
+        <option value="personal">personal</option>
+      </select>
+      </div>
+
+      {/* <h1>{task} {priority} {category}</h1> */}
+   </form>
+
+  )
 }
